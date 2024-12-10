3df1c396fade86ad957e465caae91aa082151724
flymiawoo <116167028+flymiawoo@users.noreply.github.com>
2024-12-10 09:51:14 +0300

    first commit

---
+++ app/Views/portFolio.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="global.css">
+    <link rel="icon" href="fish-bone.png">
+    <title>PortFolio</title>
+</head>
+<body>
+<div class="card card-widget widget-user">
+              <!-- Add the bg color to the header using any of the bg-* classes -->
+              <div class="widget-user-header bg-info">
+                <h3 class="widget-user-username">RATSIMISETRA Hasiniaina</h3>
+                <h5 class="widget-user-desc">Développeur Web &amp; Desktop</h5>
+              </div>
+              <div class="widget-user-image">
+                <img class="img-circle elevation-2" src="sary 2.jpg" alt="User Avatar">
+              </div>
+              <div class="card-footer">
+                <div class="row">
+                  <div class="col-sm-4 border-right">
+                    <div class="description-block">
+                      <h5 class="description-header">20</h5>
+                      <span class="description-text">Ans</span>
+                    </div>
+                    <!-- /.description-block -->
+                  </div>
+                  <!-- /.col -->
+                  <div class="col-sm-4 border-right">
+                    <div class="description-block">
+                      <h5 class="description-header">Stagiaire</h5>
+                      <span class="description-text">Informatique</span>
+                    </div>
+                    <!-- /.description-block -->
+                  </div>
+                  <!-- /.col -->
+                  <div class="col-sm-4">
+                    <div class="description-block">
+                      <h5 class="description-header">Célibataire</h5>
+                      <!-- <span class="description-text">Célibataire</span> -->
+                    </div>
+                  </div>
+                  <!-- /.col -->
+                </div>
+                <!-- /.row -->
+              </div>
+</div>
+<div class="row">
+              <div class="col-5 col-sm-3">
+                <div class="nav flex-column nav-tabs h-100" id="vert-tabs-tab" role="tablist" aria-orientation="vertical">
+                  <a class="nav-link active" id="vert-tabs-home-tab" data-toggle="pill" href="#vert-tabs-home" role="tab" aria-controls="vert-tabs-home" aria-selected="false">A propos</a>
+                  <a class="nav-link" id="vert-tabs-profile-tab" data-toggle="pill" href="#vert-tabs-profile" role="tab" aria-controls="vert-tabs-profile" aria-selected="false">Formations</a>
+                  <a class="nav-link" id="vert-tabs-messages-tab" data-toggle="pill" href="#vert-tabs-messages" role="tab" aria-controls="vert-tabs-messages" aria-selected="false">Mes Projets d'étudiants</a>
+                  <a class="nav-link" id="vert-tabs-com-tab" data-toggle="pill" href="#vert-tabs-comp" role="tab" aria-controls="vert-tabs-messages" aria-selected="false">Mes compétences</a>
+                  <a class="nav-link " id="vert-tabs-settings-tab" data-toggle="pill" href="#vert-tabs-settings" role="tab" aria-controls="vert-tabs-settings" aria-selected="true">Contacts</a>
+                </div>
+              </div>
+              <div class="col-7 col-sm-9">
+                <div class="tab-content" id="vert-tabs-tabContent">
+                  <div class="tab-pane text-left fade active show" id="vert-tabs-home" role="tabpanel" aria-labelledby="vert-tabs-home-tab">
+                  Réalisant actuellement des études en Informatique Statistique Appliquée et Intelligence Artificielle à l’ISPM (Institut Supérieur Polytechnique de Madagascar), je suis activement à la recherche d’un stage. <br>Celui-ci me permettrait non seulement d’accroître mon expérience professionnelle dans le secteur de Informatique, mais également de développer mes compétences dans l’environnement numérique.
+                  <br>Je serais plus que ravi de participer à un projet remplit de défis et d'innovation.
+                </div>
+                  <div class="tab-pane fade" id="vert-tabs-profile" role="tabpanel" aria-labelledby="vert-tabs-profile-tab">
+                     <!-- Mauris tincidunt mi at erat gravida, eget tristique urna bibendum. Mauris pharetra purus ut ligula tempor, et vulputate metus facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas sollicitudin, nisi a luctus interdum, nisl ligula placerat mi, quis posuere purus ligula eu lectus. Donec nunc tellus, elementum sit amet ultricies at, posuere nec nunc. Nunc euismod pellentesque diam. -->
+                <!-- TimeLine -->
+                <div class="col-md-12">
+                            <!-- The time line -->
+                            <div class="timeline">
+                            <!-- timeline time label -->
+                            <!-- /.timeline-label -->
+                            <!-- timeline item -->
+                            <div>
+                                <i class="fas fa-envelope bg-green"></i>
+                                <div class="timeline-item">
+                                <span class="time"><i class="fas fa-clock"></i>2023 - 2024</span>
+                                <h3 class="timeline-header"><a href="#">ISPM</a> Licence (L3) - en cours</h3>
+
+                                <div class="timeline-body">
+                                    Informatique Statistiques Appliquées et Intelligence Artificielle (ISAIA)
+                                </div>
+                                </div>
+                            </div>
+                            <!-- END timeline item -->
+                            <!-- timeline item -->
+                            <div>
+                                <i class="fas fa-user bg-blue"></i>
+                                <div class="timeline-item">
+                                <span class="time"><i class="fas fa-clock"></i>2022 - 2023</span>
+                                <h3 class="timeline-header no-border"><a href="#">ISPM</a> L2 - Bacc+2</h3>
+                                <div class="timeline-body">
+                                    Informatique Statistiques Appliquées et Intelligence Artificielle (ISAIA)
+                                </div>
+                                </div>
+                            </div>
+                            <!-- END timeline item -->
+                            <!-- timeline item -->
+                            <div>
+                                <i class="fas fa-comments bg-yellow"></i>
+                                <div class="timeline-item">
+                                <span class="time"><i class="fas fa-clock"></i>2021 - 2022</span>
+                                <h3 class="timeline-header"><a href="#">ISPM</a> L1 - Bacc+1</h3>
+                                <div class="timeline-body">
+                                    Informatique Statistiques Appliquées et Intelligence Artificielle (ISAIA)
+                                </div>
+                                </div>
+                            </div>
+                            <div>
+                                <i class="fas fa-comments bg-gray"></i>
+                                <div class="timeline-item">
+                                <span class="time"><i class="fas fa-clock"></i>2021</span>
+                                <h3 class="timeline-header"><a href="#">Antananarivo</a> Bacc </h3>
+                                <div class="timeline-body">
+                                    Bacc série D
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>           
+<!-- Timeline -->
+                  </div>
+                  <div class="tab-pane fade" id="vert-tabs-messages" role="tabpanel" aria-labelledby="vert-tabs-messages-tab">
+                     <!-- Morbi turpis dolor, vulputate vitae felis non, tincidunt congue mauris. Phasellus volutpat augue id mi placerat mollis. Vivamus faucibus eu massa eget condimentum. Fusce nec hendrerit sem, ac tristique nulla. Integer vestibulum orci odio. Cras nec augue ipsum. Suspendisse ut velit condimentum, mattis urna a, malesuada nunc. Curabitur eleifend facilisis velit finibus tristique. Nam vulputate, eros non luctus efficitur, ipsum odio volutpat massa, sit amet sollicitudin est libero sed ipsum. Nulla lacinia, ex vitae gravida fermentum, lectus ipsum gravida arcu, id fermentum metus arcu vel metus. Curabitur eget sem eu risus tincidunt eleifend ac ornare magna. -->
+<!-- Projets -->
+<div class="row">
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <!-- <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span> -->
+
+              <div class="info-box-content">
+                <span class="info-box-number">Green-Health</span>
+                <span class="text">Plateforme de gestion de patient d'un institut médical</span>
+                <span class="info-box-number">Python, PHP, MySQL</span>
+                <span class="text">L3</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <!-- <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span> -->
+
+              <div class="info-box-content">
+                <span class="info-box-number">Rent-A-Book</span>
+                <span class="text">Plateforme de gestion de librairie</span>
+                <span class="info-box-number">PHP</span>
+                <span class="text">L2</span>
+
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <!-- <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span> -->
+
+              <div class="info-box-content">
+                <span class="info-box-number">Simple Hospital</span>
+                <span class="text">Site de gestion des consultations des patients</span>
+                <span class="info-box-number">Js</span>
+                <span class="text">L1</span>
+
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+        </div>
+
+<!-- Projets -->
+        </div>
+<div class="tab-pane fade" id="vert-tabs-comp" role="tabpanel" aria-labelledby="vert-tabs-messages-tab">
+                     <!-- Morbi turpis dolor, vulputate vitae felis non, tincidunt congue mauris. Phasellus volutpat augue id mi placerat mollis. Vivamus faucibus eu massa eget condimentum. Fusce nec hendrerit sem, ac tristique nulla. Integer vestibulum orci odio. Cras nec augue ipsum. Suspendisse ut velit condimentum, mattis urna a, malesuada nunc. Curabitur eleifend facilisis velit finibus tristique. Nam vulputate, eros non luctus efficitur, ipsum odio volutpat massa, sit amet sollicitudin est libero sed ipsum. Nulla lacinia, ex vitae gravida fermentum, lectus ipsum gravida arcu, id fermentum metus arcu vel metus. Curabitur eget sem eu risus tincidunt eleifend ac ornare magna. -->
+<!-- Competences -->
+
+    <div class="row">
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-info">
+
+              <div class="info-box-content">
+                <span class="info-box-text">PHP</span>
+                <span class="info-box-number">Haut</span>
+
+                <div class="progress">
+                  <div class="progress-bar" style="width: 70%"></div>
+                </div>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-success">
+
+              <div class="info-box-content">
+                <span class="info-box-text">Python</span>
+                <span class="info-box-number">Haut</span>
+
+                <div class="progress">
+                  <div class="progress-bar" style="width: 70%"></div>
+                </div>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-warning">
+
+              <div class="info-box-content">
+                <span class="info-box-text">Java</span>
+                <span class="info-box-number">Moyen</span>
+
+                <div class="progress">
+                  <div class="progress-bar" style="width: 40%"></div>
+                </div>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-danger">
+
+              <div class="info-box-content">
+                <span class="info-box-text">C#</span>
+                <span class="info-box-number">Moyen</span>
+
+                <div class="progress">
+                  <div class="progress-bar" style="width: 30%"></div>
+                </div>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-info">
+
+              <div class="info-box-content">
+                <span class="info-box-text">HTML & CSS</span>
+                <span class="info-box-number">Haut</span>
+
+                <div class="progress">
+                  <div class="progress-bar" style="width: 80%"></div>
+                </div>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-success">
+
+              <div class="info-box-content">
+                <span class="info-box-text">Code Igniter</span>
+                <span class="info-box-number">Haut</span>
+
+                <div class="progress">
+                  <div class="progress-bar" style="width: 70%"></div>
+                </div>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-warning">
+
+              <div class="info-box-content">
+                <span class="info-box-text">MySQL</span>
+                <span class="info-box-number">Haut</span>
+
+                <div class="progress">
+                  <div class="progress-bar" style="width: 70%"></div>
+                </div>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+    </div>
+
+<!-- Competences -->
+
+</div>
+<div class="tab-pane fade" id="vert-tabs-settings" role="tabpanel" aria-labelledby="vert-tabs-settings-tab">
+ <!-- Pellentesque vestibulum commodo nibh nec blandit. Maecenas neque magna, iaculis tempus turpis ac, ornare sodales tellus. Mauris eget blandit dolor. Quisque tincidunt venenatis vulputate. Morbi euismod molestie tristique. Vestibulum consectetur dolor a vestibulum pharetra. Donec interdum placerat urna nec pharetra. Etiam eget dapibus orci, eget aliquet urna. Nunc at consequat diam. Nunc et felis ut nisl commodo dignissim. In hac habitasse platea dictumst. Praesent imperdiet accumsan ex sit amet facilisis. -->
+<!-- Footer -->
+
+<div>
+                <div class="row">
+                  <div class="col-sm-4 border-right">
+                    <div class="description-block">
+                      <h5 class="description-text">Téléphone</h5>
+                      <span class="description-header">+261 34 23 296 89</span>
+                    </div>
+                    <!-- /.description-block -->
+                  </div>
+                  <!-- /.col -->
+                  <div class="col-sm-4 border-right">
+                    <div class="description-block">
+                      <h5 class="description-text">E-mail</h5>
+                      <span class="description-header">setrahasiniaina@gmail.com</span>
+                    </div>
+                    <!-- /.description-block -->
+                  </div>
+                  <!-- /.col -->
+                  <div class="col-sm-4">
+                    <div class="description-block">
+                      <h5 class="description-text">Adresse</h5>
+                      <span class="description-header">Mahazo, Antananarivo</span>
+                    </div>
+                    <!-- /.description-block -->
+                  </div>
+                  <!-- /.col -->
+                </div>
+                <!-- /.row -->
+              </div>
+<!-- Footer -->
+
+</div>
+</div>
+</div>
+</div>
+</body>
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- <script src="../../dist/js/demo.js"></script> -->
+</html>
